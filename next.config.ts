1e9be65665338570c454840fd571b5507f5a48cf
mtchoi <mtchoi@skku.edu>
2025-12-09 20:10:59 +0900

    Refactor: Simplify next.config.ts by removing basePath and assetPrefix for production

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const basePath = process.env.GITHUB_REPOSITORY 
-  ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` 
-  : '';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd && basePath ? basePath : '',
-  assetPrefix: isProd && basePath ? basePath : '',
   images: {
     unoptimized: true,
   },
