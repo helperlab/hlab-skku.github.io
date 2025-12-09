791617b1f86218f3309f7797a969a11c195c19cf
mtchoi <mtchoi@skku.edu>
2025-12-09 20:15:06 +0900

    Remove basePath for root domain deployment (hlab-skku.github.io)

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/hlab-skku.github.io',
-  assetPrefix: '/hlab-skku.github.io',
   images: {
     unoptimized: true,
   },
